bdd43e2e8462df5cb032fe2e4e5978ff1090a205
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-28 15:34:03 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -48,7 +48,9 @@ const PlaceOrder = () => {
      try {
         const response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
         if (response.data.success) {
-          const { session_url } = response.data;
+          // const { session_url } = response.data;
+          //directly redirection without checkng
+          session_url = "https://food-delivery-frontend-no0l.onrender.com/myorders"
           window.location.replace(session_url);
         } else {
           alert("Error placing the order");
