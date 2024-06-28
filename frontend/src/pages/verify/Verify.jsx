572af7f489a2bfdf4b993ce6bde1033aa5f41145
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-28 15:45:40 +0530

    Update Verify.jsx

--- frontend/src/pages/verify/Verify.jsx
+++ frontend/src/pages/verify/Verify.jsx
@@ -1,38 +1,20 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./verify.css";
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import { StoreContext } from '../../context/StoreContext';
-import axios from 'axios';
 
 const Verify = () => {
-
-  const [searchParams , setSearchParams] = useSearchParams();
-  const success = searchParams.get("success");
-  const orderId = searchParams.get("orderId");
-  const {url} = useContext(StoreContext);
   const navigate = useNavigate();
 
-  const verifyPayment = async () => {
-    const response = await axios.post(url + "/api/order/verify" , {success , orderId} );
-    if(response.data.success){
-        navigate("/myorders");
-    }
-    else {
-      navigate("/");
-    }
-  }
-
-  useEffect( () => {
-      verifyPayment();
-  },[]);
-
+  useEffect(() => {
+    // Directly navigate to /myorders without any verification
+    navigate("/myorders");
+  }, [navigate]);
 
   return (
     <div className='verify'>
       <div className="spinner"></div> 
-
     </div>
-  )
-}
+  );
+};
 
-export default Verify
+export default Verify;
