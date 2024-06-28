873fafbdb6b88c3593e70b730b6898836f91ed18
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-28 17:10:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,7 @@ app.use("/api/user" , userRouter);
 app.use("/api/cart"  , cartRouter);
 app.use("/api/order" , orderRouter);
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
+
 
 app.get("/" , (req , res) => {
   res.send("api working")
