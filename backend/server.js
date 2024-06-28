3e8451800f1a96f174ea008305efd41b433b4dbf
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-28 16:58:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import "dotenv/config"
+import path from "path";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
@@ -22,11 +23,23 @@ app.use(cors());
 connectDB();
 
 //api endpoints
+
+
+
+
 app.use("/api/food" , foodRouter)
 app.use("/images" , express.static('uploads'));
 app.use("/api/user" , userRouter);
 app.use("/api/cart"  , cartRouter);
 app.use("/api/order" , orderRouter);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 app.get("/" , (req , res) => {
   res.send("api working")
