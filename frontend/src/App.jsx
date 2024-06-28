00ce46eb4c20ad8d007d161cd94b7998d52204ed
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-28 16:13:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,45 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home/Home';
-import Cart from './pages/Cart/Cart';
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
-import Footer from './components/Footer/Footer';
-import LoginPopUp from './components/LoginPopUp/LoginPopUp';
-import Verify from './pages/verify/Verify';
-import MyOrders from './pages/myOrders/MyOrders';
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
+import LoginPopUp from './components/LoginPopUp/LoginPopUp'
+import Verify from './pages/verify/Verify'
+import MyOrders from './pages/myOrders/MyOrders'
 
 const App = () => {
-  const [showLogin, setShowLogin] = useState(false);
+
+  const [showLogin , setShowLogin] = useState(false);
+
+
 
   return (
-    <Router>
-      <>
-        {showLogin && <LoginPopUp setShowLogin={setShowLogin} />}
-        <div className='app'>
-          <Navbar setShowLogin={setShowLogin} />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/order' element={<PlaceOrder />} />
-            <Route path='/verify' element={<Verify />} />
-            <Route path='/myorders' element={<MyOrders />} />
-          </Routes>
-        </div>
-        <Footer />
-      </>
-    </Router>
-  );
-};
+    <>
 
-export default App;
+    { showLogin? <LoginPopUp setShowLogin= {setShowLogin} />:<></>}
+
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+          <Route path="/verify" element= {<Verify/>} />
+          <Route path="/myorders" element= {<MyOrders/>} />
+        </Routes>
+
+
+
+      </div>
+      <Footer/>
+
+
+    </>
+
+  )
+}
+
+export default App
