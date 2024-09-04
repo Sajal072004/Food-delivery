32eebeca381a7ea685f401787fc1be52a8ad39ee
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-09-05 00:27:45 +0530

    done

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./navbar.css"
 import { assets } from '../../assets/assets'
+import { FaUser } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <img className='logo' src={assets.logo} alt="" />
-      <img className='profile' src={assets.profile_image} alt="" />
-
+      <FaUser></FaUser>
     </div>
   )
 }
